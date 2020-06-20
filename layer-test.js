312bc9794d527a2d1e2c31562fdbcf0b61decdd0
Drew Roos <mrgriscom@gmail.com>
2020-06-20 12:20:15 +0200

    zoom in/out from layer test

--- layer-test.js
+++ layer-test.js
@@ -7,7 +7,27 @@ function tile_coord() {
     defaults.y = Math.min(defaults.y, Math.pow(2, z) - 1);
     var x = params.get('x') || defaults.x;
     var y = params.get('y') || defaults.y;
-    return {z: z, x: x, y: y};
+    return {z: +z, x: +x, y: +y};
+}
+
+function zoom_in(coord, dx, dy) {
+    coord.z += 1;
+    coord.x = 2*coord.x + dx;
+    coord.y = 2*coord.y + dy;
+    set_coord(coord);
+}
+
+function zoom_out(coord) {
+    if (coord.z > 0) {
+        coord.z -= 1;
+        coord.x = Math.floor(coord.x / 2);
+        coord.y = Math.floor(coord.y / 2);
+        set_coord(coord);
+    }
+}
+
+function set_coord(coord) {
+    window.location.search = new URLSearchParams(coord).toString();
 }
 
 function test_tile(lyr, coord, specialcase) {
@@ -25,6 +45,18 @@ function test_tile(lyr, coord, specialcase) {
         var $container = $('#general');
         var zstr = (lyr.max_depth() != null ? 'z:' + lyr.min_depth() + '&ndash;' + lyr.max_depth() :
                     lyr.min_depth() > 0 ? 'z:' + lyr.min_depth() + '+' : '');
+
+        $(img).css('cursor', 'pointer');
+        $(img).click(function(e) {
+            zoom_in(coord,
+                    Math.floor((e.pageX - this.offsetLeft) / (TILE_SIZE / 2)),
+                    Math.floor((e.pageY - this.offsetTop) / (TILE_SIZE / 2))
+                   );
+        });
+        $(img).bind('contextmenu', function(e) {
+            zoom_out(coord);
+            return false;
+        });
     }
 
     $test = $('<div class="patch">');
